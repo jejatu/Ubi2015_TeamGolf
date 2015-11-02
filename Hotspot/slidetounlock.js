002c766b10eb559c0c874638d6924741be03b1f4
jejatu <jejatu@gmail.com>
2015-11-02 21:26:25 +0200

    Changed indentation in slidetounlock.js

--- Hotspot/slidetounlock.js
+++ Hotspot/slidetounlock.js
@@ -3,17 +3,17 @@ var nextPage = "interactive.html";
 
 $(function() {
   $("#slider").draggable({
-		axis: 'x',
-		containment: 'parent',
-		drag: function(event, ui) {
-			if (ui.position.left >= maxLeft) {
+    axis: 'x',
+    containment: 'parent',
+    drag: function(event, ui) {
+      if (ui.position.left >= maxLeft) {
         window.location = nextPage;
       }
     },
-		stop: function(event, ui) {
-			$(this).animate({
-				left: 0
-			})
-		}
-	});
+    stop: function(event, ui) {
+      $(this).animate({
+        left: 0
+      })
+    }
+  });
 });
