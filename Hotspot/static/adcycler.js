47abd98fa4526afecd3dc2df20a2daaca527d26e
jejatu <jejatu@gmail.com>
2015-11-05 22:07:58 +0200

    Ad cycler can now cycle videos

--- Hotspot/static/adcycler.js
+++ Hotspot/static/adcycler.js
@@ -1,13 +1,34 @@
-var ads = ["ads/ad1.png", "ads/ad2.png", "ads/ad3.png"];
+var type = "video";
+var images = ["ads/ad1.png", "ads/ad2.png", "ads/ad3.png"];
+var videos = ["ads/video1.mp4", "ads/video2.mp4", "ads/video3.mp4"];
 var current = 0;
 var interval = 1000;
 
-function cycle() {
-  document.getElementById('ad').src = ads[current];
-  current = (current + 1) % ads.length;
+function cycleImages() {
+  document.getElementById('image').src = images[current];
+  current = (current + 1) % images.length;
+}
+
+function cycleVideos() {
+  document.getElementById('video').src = videos[current];
+  document.getElementById('video').play();
+  current = (current + 1) % images.length;
 }
 
 window.onload = function() {
-    cycle();
-    setInterval(cycle, interval);
+  $("#image").hide();
+  $("#video").hide();
+
+  if (type === "image") {
+    $("#image").show();
+    cycleImages();
+    setInterval(cycleImages, interval);
+  }
+  else if (type === "video") {
+    $("#video").show();
+    cycleVideos();
+    document.getElementById('video').onended = function() {
+      cycleVideos();
+    };
+  }
 }
