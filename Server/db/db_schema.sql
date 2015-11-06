787cbfa757dd42c1e37b337f6705f7909ebef59f
Simon <sermler@mail.uni-mannheim.de>
2015-11-06 11:49:10 +0200

    Update sql public display

--- Server/db/db_schema.sql
+++ Server/db/db_schema.sql
@@ -4,13 +4,17 @@ CREATE TABLE IF NOT EXISTS session_data
 (
 	session_id INTEGER PRIMARY KEY AUTOINCREMENT,
 	code INTEGER UNIQUE NOT NULL,
-	time_of_session_birth TEXT,
-	time_of_code_birth TEXT
+	start_time Time,
+	end_time Time,
+	place TEXT,
+	number_of_interaction INTEGER,
+	game_score INTEGER DEFAULT NULL,
+	type_of_ad TEXT UNIQUE NOT NULL,
+	content_ad TEXT
 );
 CREATE TABLE IF NOT EXISTS survey_data
 (
 	survey_id INTEGER PRIMARY KEY AUTOINCREMENT,
 	session_id INTEGER DEFAULT NULL REFERENCES session_data(session_id) ON DELETE CASCADE
-);
 COMMIT;
 PRAGMA foreign_keys=OFF;
