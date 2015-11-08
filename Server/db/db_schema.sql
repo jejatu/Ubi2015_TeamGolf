5364415cacc4d2213099a1173297d3d5ffbad0d9
Simon <sermler@mail.uni-mannheim.de>
2015-11-08 03:53:30 +0200

    Update db survey data

--- Server/db/db_schema.sql
+++ Server/db/db_schema.sql
@@ -16,5 +16,26 @@ CREATE TABLE IF NOT EXISTS survey_data
 (
 	survey_id INTEGER PRIMARY KEY AUTOINCREMENT,
 	session_id INTEGER DEFAULT NULL REFERENCES session_data(session_id) ON DELETE CASCADE
+	notice_display BOOLEAN,
+	content_screen TEXT,
+	realize_ads BOOLEAN,
+	rating_feelings INTEGER,
+	number_of_ads INTEGER,
+	ad_content TEXT,
+	ads_Interesting BOOLEAN,
+	cause_interest TEXT,
+	ads_ annoyed BOOLEAN,
+	cause_annoying TEXT,
+	ads_attention BOOLEAN,
+	might_buy BOOLEAN,
+	ads_attention_general BOOLEAN,
+	pblic_displays_suited BOOLEAN,
+	printed_ad_worser BOOLEAN,
+	television_ad_worser BOOLEAN,
+	kind_of_ad TEXT,
+	public_display_before BOOLEAN,
+	place_Public_display TEXT,
+	remember_ad BOOLEAN
+);	
 COMMIT;
 PRAGMA foreign_keys=OFF;
