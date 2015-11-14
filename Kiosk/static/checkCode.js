45da428f47d78f4c917f022202a5ebd033348cfd
jejatu <jejatu@gmail.com>
2015-11-14 20:20:18 +0200

    Fixed some of the kiosk login code

--- Kiosk/static/checkCode.js
+++ Kiosk/static/checkCode.js
@@ -1,42 +1,40 @@
-            function checkCode()
-            {
-                var code = document.getElementById("c_input").textContent;
-                
-                if(code.length <3)
-                {
-                    $( "#c_div" ).effect( "shake" );
-                }
-                else
-                {
-                    //window.location.href = "page_1.html";
-                    var codeToPass = 'action=passcode_availability&passcode='+code;
-                    var checkSession = 'action=get_session_id&passcode='+code;
-					
-					$(document).ready(function(){
-                    $.post({ // 
-						data : codeToPass,
-						success: function(responseText){ // Get the result and associated session_id if code is correct
-                        //if no such code is found...
-                        if(responseText == 0){
-                            document.getElementById("c_input").innerHTML = "";
-                            alert("Hups, antamaasi koodia ei tunnistettu! (koodi: " + code + ")");
-                        }
-                        //if code is found...
-                        else if(responseText > 0){
-                            //get the associated session id for the given code, and pass as parameter to 1st page
-                            $.post({
-                                data: checkSession,
-                                success: function(session_id){
-                                    window.location.href = "page_1.html?session_id="+session_id;
-                                }
-                            });
-                        }
-                        //if all goes to hell
-                        else{
-                            alert('Oh noes, jotain meni vikaan tietokantayhteydessä: ' + responseText);
-                        }
-                    }
-                    });
+function checkCode()
+{
+  var code = document.getElementById("c_input").textContent;
 
-                    }
-                }
+  if (code.length == 3) {
+    //window.location.href = "page_1.html";
+    var survey = {code: code.toString()};
+
+    $(document).ready(function() {
+      $.ajax({
+        url: '/api/surveys/',
+        type: 'POST',
+        data : JSON.stringify({template: {data: survey}}),
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: function(responseText) { // Get the result and associated session_id if code is correct
+          //if no such code is found...
+          if(responseText == 0){
+              document.getElementById("c_input").innerHTML = "";
+              alert("Hups, antamaasi koodia ei tunnistettu! (koodi: " + code + ")");
+          }
+          //if code is found...
+          else if(responseText > 0){
+              //get the associated session id for the given code, and pass as parameter to 1st page
+              $.post({
+                  data: checkSession,
+                  success: function(session_id){
+                      window.location.href = "page_1.html?session_id="+session_id;
+                  }
+              });
+          }
+          //if all goes to hell
+          else{
+              alert('Oh noes, jotain meni vikaan tietokantayhteydessä: ' + responseText);
+          }
+        }
+      });
+    });
+  }
+}
