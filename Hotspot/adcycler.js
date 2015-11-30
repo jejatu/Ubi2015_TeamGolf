ded5c471a0652f8d26dc291f582c13099957fe64
Lari-Matias Orjala <lari-matias.orjala@student.oulu.fi>
2015-11-30 16:07:42 +0200

    adcycler updated.

--- Hotspot/adcycler.js
+++ Hotspot/adcycler.js
@@ -1,11 +1,20 @@
 var type = "video";
-var muted = false;
-var images = ["ads/ad1.png", "ads/ad2.png", "ads/ad3.png"];
-var videos = ["ads/video1.mp4", "ads/video2.mp4", "ads/video3.mp4"];
-var videos2 = ["ads/video1.webm", "ads/video2.webm", "ads/video3.webm"];
+var muted = true
+var images = ["ads/ad1.png", "ads/ad2.png", "ads/ad3.png", "ads/ad4.jpg",
+				"ads/ad5.jpg", "ads/ad6.jpg", "ads/ad7.jpg", "ads/ad8.png",
+				"ads/ad9.jpg", "ads/ad10.jpeg", "ads/ad11.jpg", "ads/ad12.jpg",
+				"ads/ad13.jpg", "ads/ad14.jpg", "ads/ad15.jpg"];
+var videos = ["ads/video1.mp4", "ads/video2.mp4", "ads/video3.mp4", "ads/video4.mp4",
+				"ads/video5.mp4", "ads/video6.mp4", "ads/video7.mp4", "ads/video8.mp4",
+				"ads/video9.mp4", "ads/video10.mp4", "ads/video11.mp4", "ads/video12.mp4",
+				"ads/video13.mp4", "ads/video14.mp4", "ads/video15.mp4"];
+var videos2 = ["ads/video1.webm", "ads/video2.webm", "ads/video3.webm", "ads/video4.webm",
+				"ads/video5.webm", "ads/video6.webm", "ads/video7.webm", "ads/video8.webm",
+				"ads/video9.webm", "ads/video10.webm", "ads/video11.webm", "ads/video12.webm",
+				"ads/video13.webm", "ads/video14.webm", "ads/video15.webm"];
 var current = 0;
 var interval = 6000;
-var adsStarted = [];
+var adsStarted = []
 
 function cycleImages() {
   document.getElementById('image').src = images[current];
@@ -29,23 +38,17 @@ function cycleVideos() {
 	adsStarted.push(current);
 }
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 function startAds() {
   $(".img_content").hide();
   $(".video_content").hide();
 
   if (type === "image") {
     $(".img_content").show();
-	current = getRandomInt(0, images.length - 1);
     cycleImages();
     setInterval(cycleImages, interval);
   }
   else if (type === "video") {
     $(".video_content").show();
-	current = getRandomInt(0, videos.length - 1);
     cycleVideos();
     document.getElementById('video').onended = function() {
       cycleVideos();
